8b1312372a3a107c9f495a5f2fee12c3ac84b2a7
kramayanapu <86397808+kramayanapu@users.noreply.github.com>
2021-06-24 13:06:42 +0530

    Add files via upload

---
+++ NottyNani.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NottyKoti
+{
+    class NottyNani
+    {
+        int i = 17;
+        string Name = "nani";
+        public void Method()
+        {
+            Console.WriteLine(Name);
+        }
+       public void Methodology()
+        {
+            Console.WriteLine(i);
+        }
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NottyKoti
+{
+    class Program
+    {
+        int i = 7;
+          static string Name = "nani";
+        static void Main(string[] args)
+        {
+            //Program p = new Program();
+            //Console.WriteLine("Your First Run is Success");
+            //Console.WriteLine(p.i);
+            //Console.WriteLine(Name);
+
+            NottyNani nani = new NottyNani();
+            nani.Method();
+            nani.Methodology();
+
+        }
+    }
+}
